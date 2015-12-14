bf7fab8765d25bb4ca09c00715b3644dc6b2c9c3
rankinr <robert.rankin.jR@gmail.com>
2015-12-13 23:13:52 -0600

    Fixed user agent

--- tls_bot.py
+++ tls_bot.py
@@ -118,7 +118,7 @@ br.set_handle_redirect(True)
 br.set_handle_referer(True)
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+br.addheaders = [('User-agent', 'TLS Bot, see http://mylsn.info/tlsbot.txt for more information.')]
 
 # Open some site, let's pick a random one, the first that pops in mind:
 r = br.open('http://www.top-law-schools.com/forums/ucp.php?mode=login')
