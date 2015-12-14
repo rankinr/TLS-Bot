d9ed070353b5b59cfeac17836aed306a110324f0
rankinr <robert.rankin.jR@gmail.com>
2015-12-13 22:45:35 -0600

    ED detection

--- tls_bot.py
+++ tls_bot.py
@@ -4,18 +4,18 @@
 ##SETTINGS##
 freq=60 # how often to check in seconds; will automatically sleep for 5 seconds after checking each post to avoid being blocked by PHPBB
 cron=True # if making cronjob, set this to True; will only loop once, and freq will be ignored
-username='LawSchoolBot' # TLS USERNAME FOR BOT
+username='' # TLS USERNAME FOR BOT
 password='' #TLS PASSWORD FOR BOT
 forums_to_check=['9'] # FORUMS TO CHECK - 9 = my chances (f=? in url of each forum)
 dir='/home/rob/tlsbot/' # FULL DIRECTORY THAT BOT IS IN
-urms={'URM, Exclude AA':['MA','Mexican','Latino','PR','latina','puerto','rican','native'],'AA Only':['aa','african','afam'],'Only':[],'Include':[]} #URM recognition values
+urms={'URM, Exclude AA':['MA','Mexican','Latino','PR','latina','puerto','rican','native'],'AA Only':['aa','african','afam'],'Only':['urm'],'Include':[]} #URM recognition values
 
 #MYSQL INFORMATION IS TO COORDINATE WITH MYLSN GRAPHS. CAN REMOVE MYSQL INFORMATION BY LEAVING THE VARIABLES BELOW UNCHANGED
 
-mysql_host='localhost'
-mysql_user='mylsnbot'
+mysql_host=''
+mysql_user=''
 mysql_password=''
-mysql_db='apps'
+mysql_db=''
 
 ##DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING##
 
@@ -26,7 +26,7 @@ if mysql_host != '':
 	db.autocommit(True) # Rather than reconnecting on each loop
 	cur=db.cursor()
 
-def getImageLink(lsat,gpa,urm):
+def getImageLink(lsat,gpa,urm,ed):
 	global mysql_host
 	if mysql_host != '':
 		cont=False
@@ -35,13 +35,13 @@ def getImageLink(lsat,gpa,urm):
 			cur.execute("""select %s from %s where %s="%s" limit 1;""" % ('searchkey','saved_searches','searchkey',random_string))
 			a=cur.fetchall()
 			if len(a) == 0: cont=True
-		search_params={"formsubmitted":"1","searchtype":"automatic","lsat":str(lsat),"gpa":str(gpa),"urm":urm,"ed":"Exclude","submitted1":"Show all","submitted2":"Show all","automatic":"yes","botpost":True}
+		search_params={"formsubmitted":"1","searchtype":"automatic","lsat":str(lsat),"gpa":str(gpa),"urm":urm,"ed":ed,"submitted1":"Show all","submitted2":"Show all","automatic":"yes","botpost":True}
 		cur.execute("""INSERT INTO  %s (`%s` ,`%s`,`%s`)VALUES ("%s",  "%s",  "%s");""" % ('saved_searches', 'searchkey','newvars','results',random_string,MySQLdb.escape_string(json.dumps(search_params)),''))	
 		return random_string
 	else: return ''
 
 	
-def processPost(post_content_page):
+def process_post(post_content_page):
 	post_content=post_content_page.find('div',{'class':'content'}).getText()
 	title=post_content_page.find('h2',{'class':'topic-title'}).getText()
 	#print 'http://www.top-law-schools.com/forums/viewtopic.php?f='+forum+'&t='+tid
@@ -82,16 +82,18 @@ def processPost(post_content_page):
 		for matchval in urms[urmv]:
 			if post_content.lower().count(' '+matchval.lower()+' ') != 0 or post_content.lower().count(' '+matchval.lower()+'.') != 0:
 				urm=urmv
-	return {'lsat':highest_lsat,'gpa':highest_gpa,'urm':urm,'posts':num_posts}
+	ed='Exclude'
+	if post_content.lower().count(' ed ') != 0 or post_content.lower().count('early decision') != 0: ed='Only'
+	return {'lsat':highest_lsat,'gpa':highest_gpa,'urm':urm,'posts':num_posts,'ed':ed}
 
-def generate_reply(lsat,gpa,urm,num_posts):
+def generate_reply(lsat,gpa,urm,num_posts,ed):
 	reply_text=[]
 	if num_posts < 10: reply_text.append('Welcome to TLS!')
-	if highest_gpa == 10 or highest_lsat == 0: reply_text.append("It doesn't look like you've told us your LSAT and GPA. Many factors might affect your law school applications, but most TLS users agree that LSAT and GPA are usually the two most important. Please edit your post to include your LSAT and LSAC GPA. If you have not yet taken the LSAT, a range of practice test scores is okay, too.")
+	if gpa == 10 or lsat == 0: reply_text.append("It doesn't look like you've told us your LSAT and GPA. Many factors might affect your law school applications, but most TLS users agree that LSAT and GPA are usually the two most important. Please edit your post to include your LSAT and LSAC GPA. If you have not yet taken the LSAT, a range of practice test scores is okay, too.")
 	else:
-		image_link=getImageLink(highest_lsat,highest_gpa,urm)
-		# ONCE CAN POST IMAGES: reply_text.append("Since you've provided your LSAT ("+str(highest_lsat)+") and GPA ("+str(highest_gpa)+"), I have generated an image with an overview of relevant admissions data on [url=http://lawschoolnumbers.com]LawSchoolNumbers.com[/url].\n\nHere is your image: http://mylsn.info/"+image_link+"_1-14.jpg")
-		reply_text.append("Since you've provided your LSAT ("+str(highest_lsat)+") and GPA ("+str(highest_gpa)+"), I have generated a graph with an overview of relevant admissions data on [url=http://lawschoolnumbers.com]LawSchoolNumbers.com[/url].\n\nHere is your image: http://mylsn.info/"+image_link)
+		image_link=getImageLink(lsat,gpa,urm,ed)
+		# ONCE CAN POST IMAGES: reply_text.append("Since you've provided your LSAT ("+str(lsat)+") and GPA ("+str(gpa)+"), I have generated an image with an overview of relevant admissions data on [url=http://lawschoolnumbers.com]LawSchoolNumbers.com[/url].\n\nHere is your image: http://mylsn.info/"+image_link+"_1-14.jpg")
+		reply_text.append("Since you've provided your LSAT ("+str(lsat)+") and GPA ("+str(gpa)+"), I have generated a graph with an overview of relevant admissions data on [url=http://lawschoolnumbers.com]LawSchoolNumbers.com[/url].\n\nHere is your image: http://mylsn.info/"+image_link)
 	reply_text.append("Other useful links:\n[list][*][url=http://lawschoolnumbers.com]LawSchoolNumbers[/url]: Self-Reported Law School Admissions Data\n[*][url=http://mylsn.info]MyLSN[/url]: Aggregates Data from LawSchoolNumbers.com\n[*][url=http://www.lawschoolpredictor.com/wp-content/uploads/Law-School-Predictor-LSDAS-GPA-Calculator.htm]LSAC GPA Calculator[/url]: most TLS'ers consider LSAC GPA to be the most important measure of GPA\n[*][url=http://www.lstscorereports.com/national/]LawSchoolTransparency[/url]: Law School Employment Data[/list]")
 	reply_text.append("This post is just meant to provide you with a bit of helpful starting information. Hopefully more users will be along shortly to help you more.")
 	reply_text.append("[size=85][i]I am a bot, but my inbox is actively monitored. If you have any questions, comments, suggestions, or criticisms, please send a message to this account. I can not yet post images, but when I reach 100 posts I will post them instead of links.[/i][/size]")	
@@ -151,7 +153,7 @@ while keepGoing:
 				if int(tid) > int(start_most_recent_check): # was this thread posted after the most recent one we checked?
 					if int(tid) > int(most_recent_check): most_recent_check=tid # update most recently checked thread
 					post_data=process_post(BeautifulSoup(br.open('http://www.top-law-schools.com/forums/viewtopic.php?f='+forum+'&t='+tid).read())) # FIGURE OUT LSAT, GPA, URM
-					reply_text=generate_reply(post_data['lsat'],post_data['gpa'],post_data['urm'],post_data['posts']) # GENERATE REPLY TEXT
+					reply_text=generate_reply(post_data['lsat'],post_data['gpa'],post_data['urm'],post_data['posts'],post_data['ed']) # GENERATE REPLY TEXT
 					doPost(forum,tid,reply_text) # POST REPLY TEXT
 					time.sleep(2)
 		if start_most_recent_check != most_recent_check: open(dir+'most_recent_check','w').write(most_recent_check) # update latest topic id checked for future runs
